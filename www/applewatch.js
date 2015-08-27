5f840435d2c2193364be7dc408146e5d4816098f
Jesse MacFadyen <purplecabbage@gmail.com>
2015-08-27 10:59:57 -0700

    Removed object/prototype as there is only ever one AppleWatch.
    Added a todo: for the caught JSON.parse error, should output something at least,.

--- www/applewatch.js
+++ www/applewatch.js
@@ -1,78 +1,58 @@
 
 var exec = require("cordova/exec");
 
-var AppleWatch = function () {
-    this.name = "AppleWatch";
-};
-
-AppleWatch.prototype.init = function (onSuccess, onError, appGroupId) {
-    exec(onSuccess, onError, "AppleWatch", "init", [{
-        "appGroupId": appGroupId
-    }]);
-};
-
-AppleWatch.prototype.registerNotifications = function (onSuccess, onError) {
-    exec(onSuccess, onError, "AppleWatch", "registerNotifications", []);
-};
-
-AppleWatch.prototype.sendMessage = function (message, queueName, onSuccess, onError) {
-    if (typeof(message) === "object") {
-        message = JSON.stringify(message);
+module.exports = {
+    name"AppleWatch",
+    init:function (onSuccess, onError, appGroupId) {
+        exec(onSuccess, onError, "AppleWatch", "init", [{"appGroupId": appGroupId}])
+    },
+    registerNotifications:function (onSuccess, onError) {
+        exec(onSuccess, onError, "AppleWatch", "registerNotifications", []);
+    },
+    sendMessage:function (message, queueName, onSuccess, onError) {
+        if (typeof(message) === "object") {
+            message = JSON.stringify(message);
+        }
+        exec(onSuccess, onError, "AppleWatch", "sendMessage", [{"queueName": queueName, "message": message }]);
+    },
+    sendNotification:function (onSuccess, onError, payload) {
+        exec(onSuccess, onError, "AppleWatch", "sendNotification", [payload]);
+    },
+    sendUserDefaults:function (onSuccess, onError, obj, appGroupId) {
+        var key = Object.keys(obj)[0];
+        var payload = {
+            "key": key,
+            "value": obj[key],
+            "appGroupId": appGroupId
+        };
+        exec(onSuccess, onError, "AppleWatch", "sendUserDefaults", [payload]);
+    },
+    getUserDefaults:function (onSuccess, onError, key, appGroupId) {
+        var payload = {
+            "key": key,
+            "appGroupId": appGroupId
+        };
+        exec(onSuccess, onError, "AppleWatch", "getUserDefaults", [payload]);
+    },
+    addListener:function (queueName, onMessage) {
+        var wrappedOnMessage = function (message) {
+            try { 
+                message = JSON.parse(message); 
+            } 
+            catch (e) {
+                // TODO:
+            }
+            onMessage(message);
+        };
+        exec(wrappedOnMessage, null, "AppleWatch", "addListener", [{"queueName": queueName }]);
+    },
+    removeListener:function (queueName, onSuccess, onError) {
+        exec(onSuccess, onError, "AppleWatch", "removeListener", [{"queueName": queueName}]);
+    },
+    purgeQueue:function (queueName, onSuccess, onError) {
+        exec(onSuccess, onError, "AppleWatch", "purgeQueue", [{"queueName": queueName}]);
+    },
+    purgeAllQueues:function (onSuccess, onError) {
+        exec(onSuccess, onError, "AppleWatch", "purgeAllQueues", []);
     }
-
-    exec(onSuccess, onError, "AppleWatch", "sendMessage", [{
-        "queueName": queueName,
-        "message": message
-    }]);
 };
-
-AppleWatch.prototype.sendNotification = function (onSuccess, onError, payload) {
-    exec(onSuccess, onError, "AppleWatch", "sendNotification", [payload]);
-};
-
-AppleWatch.prototype.sendUserDefaults = function (onSuccess, onError, obj, appGroupId) {
-    var key = Object.keys(obj)[0];
-    var payload = {
-        "key": key,
-        "value": obj[key],
-        "appGroupId": appGroupId
-    };
-    exec(onSuccess, onError, "AppleWatch", "sendUserDefaults", [payload]);
-};
-
-AppleWatch.prototype.getUserDefaults = function (onSuccess, onError, key, appGroupId) {
-    var payload = {
-        "key": key,
-        "appGroupId": appGroupId
-    };
-    exec(onSuccess, onError, "AppleWatch", "getUserDefaults", [payload]);
-};
-
-AppleWatch.prototype.addListener = function (queueName, onMessage) {
-    var wrappedOnMessage = function (message) {
-        try { message = JSON.parse(message); } catch (e) {}
-        onMessage(message);
-    };
-
-    exec(wrappedOnMessage, null, "AppleWatch", "addListener", [{
-        "queueName": queueName
-    }]);
-};
-
-AppleWatch.prototype.removeListener = function (queueName, onSuccess, onError) {
-    exec(onSuccess, onError, "AppleWatch", "removeListener", [{
-        "queueName": queueName
-    }]);
-};
-
-AppleWatch.prototype.purgeQueue = function (queueName, onSuccess, onError) {
-    exec(onSuccess, onError, "AppleWatch", "purgeQueue", [{
-        "queueName": queueName
-    }]);
-};
-
-AppleWatch.prototype.purgeAllQueues = function (onSuccess, onError) {
-    exec(onSuccess, onError, "AppleWatch", "purgeAllQueues", []);
-};
-
-module.exports = new AppleWatch();
